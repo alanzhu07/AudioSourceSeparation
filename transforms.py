1d0fe4ffbd226eae10fdfad9e08d8cdad48162e5
alanzhu07 <alanzhuyixuan@gmail.com>
2022-10-30 13:51:04 -0400

    MDenseNet baseline model

--- transforms.py
+++ transforms.py
@@ -5,18 +5,26 @@ def stft(input,
     hop_length=1024,
     win_length=4096,
     window=torch.hann_window(4096),
+    to_complex=True,
     center=True):
     """
     input <torch.Tensor>: (num_samples x num_channels x length)
 
-    output <torch.Tensor>: (num_samples x num_channels x fft_size x n_fft_frames)
+    output <torch.Tensor>:
+        (num_samples x num_channels x fft_size x n_fft_frames) if to_complex
+        else (num_samples x num_channels, 2, fft_size, n_fft_frames) 
     """
 
     num_samples, num_channels, length = input.size()
     input = input.view(-1, length)
 
-    stft = torch.stft(input, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window, center=center, return_complex=True)
-    stft = stft.view(num_samples, num_channels, *stft.size()[-2:])
+    if to_complex:
+        stft = torch.stft(input, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window, center=center, return_complex=True)
+        stft = stft.view(num_samples, num_channels, *stft.size()[-2:])
+    else:
+        stft = torch.stft(input, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window, center=center, return_complex=False)
+        stft = stft.movedim(-1,1)
+        # stft = stft.view(num_samples*num_channels, 2, *stft.size()[-2:])
 
     return stft
 
@@ -25,15 +33,23 @@ def istft(input,
     hop_length=1024,
     win_length=4096,
     window=torch.hann_window(4096),
-    center=True):
+    center=True,
+    from_complex=True,
+    num_channels=2):
     """
     input <torch.Tensor>: (num_samples x num_channels x fft_size x n_fft_frames)
 
     output <torch.Tensor>: (num_samples x num_channels x length)
     """
-
-    num_samples, num_channels, fft_size, n_fft_frames = input.size()
-    input = input.view(-1, fft_size, n_fft_frames)
+    
+    if from_complex:
+        num_samples, num_channels, fft_size, n_fft_frames = input.size()
+        input = input.view(-1, fft_size, n_fft_frames)
+    else:
+        num_samples_, _, fft_size, n_fft_frames = input.size()
+        num_samples = num_samples_ // num_channels
+        input = input.movedim(1,-1)
+        input = torch.view_as_complex(input)
 
     istft = torch.istft(input, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window, center=center, return_complex=False)
     istft = istft.view(num_samples, num_channels, -1)
--- utils.py
+++ utils.py
@@ -2,10 +2,14 @@ import numpy as np
 
 def sample_seconds(audio, sample_rate, seconds, rng=None):
     """
-    audio (np.ndarray): (num_channels, length)
+    audio : (np.ndarray or List[np.ndarray]) (num_channels, length)
     """
 
-    original_length = audio.shape[1]
+    if type(audio) is list:
+        original_length = audio[0].shape[1]
+    else:
+        original_length = audio.shape[1]
+
     sample_length = sample_rate*seconds
     if original_length < sample_length:
         raise Exception("audio too short")
@@ -13,26 +17,38 @@ def sample_seconds(audio, sample_rate, seconds, rng=None):
     if not rng:
         rng = np.random.default_rng()
     i = rng.integers(original_length-sample_length)
-    output = audio[:,i:i+sample_length]
+
+    if type(audio) is list:
+        output = [a[:,i:i+sample_length] for a in audio]
+    else:
+        output = audio[:,i:i+sample_length]
 
     return output
 
 def sample_frames(audio, sample_length, rng=None):
 
-    original_length = audio.shape[1]
+    if type(audio) is list:
+        original_length = audio[0].shape[1]
+    else:
+        original_length = audio.shape[1]
+
     if original_length < sample_length:
         raise Exception("audio too short")
 
     if not rng:
         rng = np.random.default_rng()
     i = rng.integers(original_length-sample_length)
-    output = audio[:,i:i+sample_length]
+
+    if type(audio) is list:
+        output = [a[:,i:i+sample_length] for a in audio]
+    else:
+        output = audio[:,i:i+sample_length]
 
     return output
 
 def sample_tracks(tracks, sample_rate, seconds, rng=None):
     """
-    tracks: List[np.ndarray()]
+    tracks: List[np.ndarray()] or List[List[np.ndarray()]]
 
     output: (num_tracks, num_channels, sampled_length)
     """
@@ -42,17 +58,28 @@ def sample_tracks(tracks, sample_rate, seconds, rng=None):
 
     return output
 
-def sample_musdb_tracks(tracks, seconds, rng=None):
+def sample_musdb_tracks(tracks, seconds, sample_targets="vocals", rng=None):
     """
     tracks: musdb.audio_classes.Track
 
     output: (num_tracks, num_channels, sampled_length)
     """
 
-    output = []
-    for track in tracks:
-        output.append(sample_seconds(track.audio.T, track.rate, seconds, rng=rng))
-    output = np.array(output)
+    if not sample_targets:
+        output = []
+        for track in tracks:
+            output.append(sample_seconds(track.audio.T, track.rate, seconds, rng=rng))
+        output = np.array(output)
 
-    return output
+        return output
+    else:
+        mixtures = []
+        targets = []
+        for track in tracks:
+            mixture_sampled, target_sampled = sample_seconds([track.audio.T, track.targets[sample_targets].audio.T], track.rate, seconds, rng=rng)
+            mixtures.append(mixture_sampled)
+            targets.append(target_sampled)
+        mixtures, targets = np.array(mixtures), np.array(targets)
+
+        return mixtures, targets
     
