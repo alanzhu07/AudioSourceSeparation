7f0794c48d364ad9035f6e5373fdc5c7d406d9f7
alanzhu07 <alanzhuyixuan@gmail.com>
2022-11-26 03:31:16 +0000

    add transforms.py compatibility with cuda

--- transforms.py
+++ transforms.py
@@ -6,7 +6,8 @@ def stft(input,
     win_length=4096,
     window=torch.hann_window(4096),
     to_complex=True,
-    center=True):
+    center=True,
+    device='cuda'):
     """
     input <torch.Tensor>: (num_samples x num_channels x length)
 
@@ -17,6 +18,7 @@ def stft(input,
 
     num_samples, num_channels, length = input.size()
     input = input.view(-1, length)
+    window = window.to(input.device)
 
     if to_complex:
         stft = torch.stft(input, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window, center=center, return_complex=True)
@@ -26,7 +28,7 @@ def stft(input,
         stft = stft.movedim(-1,1)
         # stft = stft.view(num_samples*num_channels, 2, *stft.size()[-2:])
 
-    return stft
+    return stft.to(device)
 
 def istft(input,
     n_fft=4096,
@@ -35,13 +37,16 @@ def istft(input,
     window=torch.hann_window(4096),
     center=True,
     from_complex=True,
-    num_channels=2):
+    num_channels=2,
+    device='cuda'):
     """
     input <torch.Tensor>: (num_samples x num_channels x fft_size x n_fft_frames)
 
     output <torch.Tensor>: (num_samples x num_channels x length)
     """
     
+    window = window.to(input.device)
+
     if from_complex:
         num_samples, num_channels, fft_size, n_fft_frames = input.size()
         input = input.view(-1, fft_size, n_fft_frames)
@@ -54,4 +59,4 @@ def istft(input,
     istft = torch.istft(input, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window, center=center, return_complex=False)
     istft = istft.view(num_samples, num_channels, -1)
 
-    return istft
+    return istft.to(device)
